c227c3fd65cfa1d1f42b5e30f6ecd492f55634fa
Cherry <sarahandecherry@gmail.com>
2023-06-23 10:14:40 -0700

    endpoint stubs, create + read methods, request rspec for create + read

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,26 @@
 class TripsController < ApplicationController
+    def index
+        trips = Trip.all
+        render json: trips
+    end
+
+    def create
+        trip = Trip.create(trip_params)
+        if trip.valid?
+            render json: trip
+        else
+            render json: trip.errors, status: 422
+        end
+    end
+    
+    def update
+    end
+    
+    def destroy
+    end
+
+    private
+    def trip_params
+        params.require(:trip).permit(:title, :entry, :photo, :start_date, :end_date, :city, :state, :country, :region, :international, :user_id)
+    end
 end
--- spec/requests/trips_spec.rb
+++ spec/requests/trips_spec.rb
@@ -1,7 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe "Trips", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
-  end
+    # test user created and locally scoped
+    let(:user) { User.create(
+        email: 'test@example.com',
+        password: 'password',
+        password_confirmation: 'password'
+    )}
+
+    # validates request method GET
+    describe "GET /index" do
+        it "gets a list of trips" do
+            trip = user.trips.create(
+                title: "River rafting trip",
+                entry: "This was such a great trip! We did some rafting down the Arkansas and Poudre rivers, then hit the Colorado river with packrafts for a weekend long float. We camped riverside overnight and then hopped out in Moab, Utah. Good rapids, developed skills. Need a throw rope and more practice with recovering from big raft flips.",
+                country: "United States",
+                region:"North America",
+                user_id: user.id
+            )
+            get '/trips'
+
+            trip = JSON.parse(response.body)
+            expect(response).to have_http_status(200)
+            expect(trip.first['title']).to eq("River rafting trip")
+            expect(trip.first['entry']).to eq("This was such a great trip! We did some rafting down the Arkansas and Poudre rivers, then hit the Colorado river with packrafts for a weekend long float. We camped riverside overnight and then hopped out in Moab, Utah. Good rapids, developed skills. Need a throw rope and more practice with recovering from big raft flips.")
+            expect(trip.first['country']).to eq("United States")
+            expect(trip.first['region']).to eq("North America")
+        end
+    end
+
+    # validates request method POST
+    describe "POST /create" do
+        it "creates a new trip" do
+            trip_params = {
+                trip: {
+                    title: "River rafting trip",
+                    entry: "This was such a great trip! We did some rafting down the Arkansas and Poudre rivers, then hit the Colorado river with packrafts for a weekend long float. We camped riverside overnight and then hopped out in Moab, Utah. Good rapids, developed skills. Need a throw rope and more practice with recovering from big raft flips.",
+                    country: "United States",
+                    region:"North America",
+                    user_id: user.id
+                }
+            }
+            post '/trips', params: trip_params
+
+            json = JSON.parse(response.body)
+            expect(json['title']).to eq("River rafting trip")
+            expect(json['entry']).to eq("This was such a great trip! We did some rafting down the Arkansas and Poudre rivers, then hit the Colorado river with packrafts for a weekend long float. We camped riverside overnight and then hopped out in Moab, Utah. Good rapids, developed skills. Need a throw rope and more practice with recovering from big raft flips.")
+            expect(json['country']).to eq("United States")
+            expect(json['region']).to eq("North America")
+        end
+    end
+    
 end
