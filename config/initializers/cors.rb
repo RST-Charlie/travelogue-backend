1e8ed753265f106a1f9aab3c4c845a81ef698e03
Cherry <sarahandecherry@gmail.com>
2023-06-30 10:32:05 -0700

    adjust origins link

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
         origins 'travelogue-app.onrender.com'
         resource '*',
-        headers: ["Authorization", "Access-Control-Allow-Origin: *"],
+        headers: ["Authorization", "Access-Control-Allow-Origin"],
         expose: ["Authorization"],
         methods: [:get, :post, :put, :patch, :delete, :options, :head],
         max_age: 600
